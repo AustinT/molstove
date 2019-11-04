2522fe377f456128c1f16bb739cb2584d8d35d3b
Gregor Simm <gncsimm@gmail.com>
2019-11-04 10:14:21 +0000

    simplify analysis script

--- scripts/analyze_photo.py
+++ scripts/analyze_photo.py
@@ -5,9 +5,6 @@ import os
 import numpy as np
 import pandas as pd
 
-from molstove import tools
-from molstove.properties import calibrate_homo, calibrate_lumo, calculate_scharber_props
-
 reference = {
     'c1coc(c1)-c1cc2sc3c(c4c[nH]cc4c4ccc5cscc5c34)c2c2ccccc12': 2.110,
     '[SiH2]1C=c2c3cc[nH]c3c3ccc4cc(-c5scc6cc[nH]c56)c5cscc5c4c3c2=C1': 2.620,
@@ -22,14 +19,6 @@ reference = {
 }
 
 
-def new_scharber(row: pd.Series) -> float:
-    method = f'BP86/def2-SVP//{row["xc"]}/def2-SVP'
-    homo = calibrate_homo(homo=row['homo'] * tools.EV_PER_HARTREE, method=method)
-    lumo = calibrate_lumo(lumo=row['lumo'] * tools.EV_PER_HARTREE, method=method)
-
-    return calculate_scharber_props(homo=homo, lumo=lumo).pce
-
-
 def main(directory: str) -> None:
     # Collect all JSON files
     json_files = glob.glob(os.path.join(directory, 'report_*.json'))
@@ -39,9 +28,6 @@ def main(directory: str) -> None:
         frames.append(pd.read_json(file))
 
     df = pd.concat(frames)
-    df = df[df['xc'] != 'PBE']
-
-    df['pce_calibrated'] = df.apply(axis='columns', func=new_scharber)
 
     # Join with reference data
     reference_series = pd.Series(reference)
@@ -50,11 +36,12 @@ def main(directory: str) -> None:
     df = df.join(reference_series, how='left', on='smiles')
     df = df.reset_index()
 
+    # Compute mean
     df = df.groupby('smiles').agg(np.mean)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None, 'max_colwidth',
                            100):
-        print(df[['pce_calibrated', 'pce_ref']])
+        print(df[['pce', 'pce_ref']])
 
 
 if __name__ == '__main__':
