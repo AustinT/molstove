5709c8b599f6d92afd48bf9e92ec6d2862c0665d
Gregor Simm <gncsimm@gmail.com>
2019-12-13 13:39:00 +0000

    analyse additional calculations for structures from supplementary information

--- scripts/analyze_photo.py
+++ scripts/analyze_photo.py
@@ -1,9 +1,14 @@
 import argparse
 import glob
+import json
 import os
 
 import numpy as np
 import pandas as pd
+
+from molstove import tools
+from molstove.parser import OrcaParser
+from molstove.properties import calibrate_homo, calibrate_lumo, calculate_scharber_props
 
 reference = {
     'c1coc(c1)-c1cc2sc3c(c4c[nH]cc4c4ccc5cscc5c34)c2c2ccccc12': 2.110,
@@ -19,15 +24,45 @@ reference = {
 }
 
 
+# # From: http://www.rsc.org/suppdata/ee/c3/c3ee42756k/c3ee42756k.pdf
+# reference = {
+#     'c1sc(-c2ccc(cn2)-c2[SiH2]c(cc2)-c2nccc3nsnc23)c2sccc12': 11.13,
+#     'c1cnc(-c2cnc3c(c2)c2=C[SiH2]C=c2c2ccc4cscc4c32)c2nsnc12': 11.13,
+#     'c1cc2ncc3c4c5cocc5c(cc4c4=C[SiH2]C=c4c3c2o1)-c1nccc2nsnc12': 11.12,
+#     'c1ccc(-c2cc3c4nsnc4c4ccc5=C[SiH2]C=c5c4c3c3nsnc23)c2=C[SiH2]C=c12': 11.12,
+# }
+
+
+def recalculate_scharber(row: pd.Series, directory: str):
+    calc_dir = os.path.join(directory, row['path'])
+    p = OrcaParser(directory=calc_dir, output_file_name='orca.output')
+    p.sanity_check()
+    if 'UHF' in row['method']:
+        orbitals = p.get_last_open_shell_orbitals()
+        homo, lumo = tools.get_homo_lumo_energies(orbitals, is_open_shell=True)
+    else:
+        orbitals = p.get_last_orbitals()
+        homo, lumo = tools.get_homo_lumo_energies(orbitals, is_open_shell=False)
+    homo_calibrated = calibrate_homo(homo=homo * tools.EV_PER_HARTREE, method=row['method'])
+    lumo_calibrated = calibrate_lumo(lumo=lumo * tools.EV_PER_HARTREE, method=row['method'])
+    props = calculate_scharber_props(homo=homo_calibrated, lumo=lumo_calibrated, e_charge_sep=0)
+    return props.pce
+
+
 def main(directory: str) -> None:
     # Collect all JSON files
     json_files = glob.glob(os.path.join(directory, 'report_*.json'))
 
     frames = []
     for file in json_files:
-        frames.append(pd.read_json(file))
+        with open(file) as f:
+            data = json.load(f)
+            frames.append(pd.DataFrame(data))
 
     df = pd.concat(frames)
+
+    # Determine PCE using different settings
+    df['pce_alt'] = df.apply(func=lambda row: recalculate_scharber(row, directory=directory), axis='columns')
 
     # Join with reference data
     reference_series = pd.Series(reference)
@@ -40,8 +75,8 @@ def main(directory: str) -> None:
     df = df.groupby('smiles').agg(np.mean)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None, 'max_colwidth',
-                           100):
-        print(df[['pce', 'pce_ref']])
+                           200):
+        print(df[['pce', 'pce_alt', 'pce_ref']])
 
 
 if __name__ == '__main__':
